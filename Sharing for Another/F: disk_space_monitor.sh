898e6069d1133495254f4bc1006792b846df2398
terminalforlife <terminalforlife@yahoo.com>
2017-10-03 19:39:14 +0100

    Updated F\:\ disk_space_monitor.sh

--- Sharing for Another/F: disk_space_monitor.sh
+++ Sharing for Another/F: disk_space_monitor.sh
@@ -8,14 +8,13 @@ check_interval=3
 while true; do
   # Percentage of disk space used
   used_space=$(read <<< `df -H --output=pcent /`; echo ${REPLY/* })
-    
+
   if [ $used_space -ge $warning_level ]
   then
     notify-send "System drive is ${used_space} full               " \
       -i dialog-warning -t 36000000 -u critical
     exit
   fi
-  
+
   sleep ${check_interval}m
 done
-
