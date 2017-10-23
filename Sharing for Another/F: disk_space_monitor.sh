0fc569f5809e013608f18d46f782e9e12af85956
terminalforlife <terminalforlife@yahoo.com>
2017-10-23 01:24:36 +0100

    Updated F\:\ disk_space_monitor.sh

--- Sharing for Another/F: disk_space_monitor.sh
+++ Sharing for Another/F: disk_space_monitor.sh
@@ -7,7 +7,7 @@ check_interval=3
 
 while true; do
   # Percentage of disk space used
-  used_space=$(read <<< `df -H --output=pcent /`; echo ${REPLY/* })
+  used_space=$(read -a X <<< `/bin/df /`; echo ${X[11]})
 
   if [ $used_space -ge $warning_level ]
   then
