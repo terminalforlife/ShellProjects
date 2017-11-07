9f4dc66e27b242a2ce964df5594c0a0656399ae0
terminalforlife <terminalforlife@yahoo.com>
2017-11-07 20:25:07 +0000

    Updated files

---
+++ update_links.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#----------------------------------------------------------------------------------
+# Project Name      - miscellaneous/update_links.sh
+# Started On        - Sun 22 Oct 00:15:02 BST 2017
+# Last Change       - Tue  7 Nov 20:22:16 GMT 2017
+# Author E-Mail     - terminalforlife@yahoo.com
+# Author GitHub     - https://github.com/terminalforlife
+#----------------------------------------------------------------------------------
+
+# Just a simple, quick script to update the hard links when changing branches.
+
+#------------------------------------------------------------------------------MAIN
+
+XERR(){ printf "[L%0.4d] ERROR: %s\n" "$1" "$2" 1>&2; exit 1; }
+ERR(){ printf "[L%0.4d] ERROR: %s\n" "$1" "$2" 1>&2; }
+
+declare -i DEPCOUNT=0
+for DEP in /bin/{ln,rm}; {
+	[ -x "$DEP" ] || {
+		ERR "$LINENO" "Dependency '$DEP' not met."
+		DEPCOUNT+=1
+	}
+}
+
+[ $DEPCOUNT -eq 0 ] || exit 1
+
+[ "${PWD//*\/}" == "miscellaneous" ] || {
+	XERR "$LINENO" "Not in the repository's root directory."
+}
+
+/bin/rm $HOME/.config/compton.conf 2> /dev/null
+/bin/ln compton.conf $HOME/.config/ 2> /dev/null
+
+/bin/rm $HOME/.keynavrc 2> /dev/null
+/bin/ln .keynavrc $HOME/ 2> /dev/null
+
+/bin/rm $HOME/.vim/colors/tfl.vim 2> /dev/null
+/bin/ln tfl.vim $HOME/.vim/colors/ 2> /dev/null
+
+/bin/rm $HOME/.vimrc 2> /dev/null
+/bin/ln .vimrc $HOME/ 2> /dev/null
