1776b5af0b31b6bb3b72ae5d34bdae9e1f5eb7d6
terminalforlife <terminalforlife@yahoo.com>
2019-04-26 14:38:33 +0100

    Updated config.fish; no more /dev/null

--- config.fish
+++ config.fish
@@ -3,14 +3,14 @@
 #----------------------------------------------------------------------------------
 # Project Name      - $HOME/.config/fish/config.fish
 # Started On        - Thu 14 Sep 12:44:56 BST 2017
-# Last Change       - Fri 22 Sep 20:09:34 BST 2017
+# Last Change       - Fri 26 Apr 14:38:20 BST 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
 
 if [ -d "$HOME/bin" ]; and [ "$PATH" != */home/"$USER"/bin* ]
 	export PATH="/home/$USER/bin $PATH"
-end 
+end
 
 umask 0077
 
@@ -28,7 +28,7 @@ if [ -d "$FLIB" ]
 		Load_File_Links2 CPU_Intensive_Procs Git_Status_All;
 		[ -f "$FLIB/$FUNC" ]; and source "$FLIB/$FUNC"
 	end
-end 
+end
 
 #unset FLIB FUNC
 
@@ -42,33 +42,33 @@ export LESSSECURE=1
 export COLUMNS
 export LINES
 
-if type -P /usr/bin/sudo 2>&1 > /dev/null
-	if type -P /usr/bin/vim 2>&1 > /dev/null
+if type -P /usr/bin/sudo >&- 2>&-
+	if type -P /usr/bin/vim >&- 2>&-
 		export SUDO_EDITOR="rvim"
-	else if type -P /usr/bin/nano 2>&1 > /dev/null
+	else if type -P /usr/bin/nano >&- 2>&-
 		export SUDO_EDITOR="rnano"
-	end 
-end 
+	end
+end
 
 #----------------------------------------------------------------------------------
 
-if type -P /bin/date /usr/bin/tty 2>&1 > /dev/null
+if type -P /bin/date /usr/bin/tty >&- 2>&-
 	set TERMWATCH_LOGFILE "$HOME/.termwatch.log"
 	set CURTERM `/usr/bin/tty`
 
 	if [ -f "$TERMWATCH_LOGFILE" ]; and [ -w "$TERMWATCH_LOGFILE" ]
 		echo "Using $CURTERM:-Unknown ($TERM-unknown)"\
 			"at `/bin/date` as $USER." >> "$TERMWATCH_LOGFILE"
-	end 
+	end
 
 	#unset TERMWATCH_LOGFILE CURTERM
-end 
+end
 
 #----------------------------------------------------------------------------------
 
 set FISH_ALIASES "$HOME/.fish_aliases"
 if [ -f "$FISH_ALIASES" ]; and [ -r "$FISH_ALIASES" ]
 	source "$FISH_ALIASES"
-end 
+end
 
 #unset FISH_ALIASES
