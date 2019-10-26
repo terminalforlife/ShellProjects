a19a86303bd6fd5a364a9e73e79fba849e3a3262
terminalforlife <terminalforlife@yahoo.com>
2019-10-26 23:52:43 +0100

    Added update_versions.sh

---
+++ update_versions.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#----------------------------------------------------------------------------------
+# Project Name      - update_versions.sh
+# Started On        - Fri 25 Oct 12:41:34 BST 2019
+# Last Change       - Sat 26 Oct 23:52:15 BST 2019
+# Author E-Mail     - terminalforlife@yahoo.com
+# Author GitHub     - https://github.com/terminalforlife
+#----------------------------------------------------------------------------------
+# Fetches then saves versions deemed by current-directory shell prorams' initial
+# _VERSION_ variable, in key=value format. Parsing only; no program is executed.
+#----------------------------------------------------------------------------------
+
+VER_FILE='./versions'
+
+FAIL(){
+	printf "[L%0.4d] ERROR: %s\n" "$2" "$3" 1>&2
+	[ $1 -eq 1 ] && exit 1
+}
+
+printf "%s " "Confirming 'mimetype' exists..."
+
+if ! type -fP mimetype &> /dev/null; then
+	FAIL 1 "$LINENO" "Unable to find 'mimetype' in PATH."
+else
+	printf " [OK]\n"
+fi
+
+printf "%s " "Gathering version strings..."
+
+DATA="$(\
+	for F in *; {
+		MIME=`mimetype -b "$F" 2> /dev/null`
+		if [[ "$MIME" == *x-shellscript && ! "$F" == update_*.sh ]]; then
+			while IFS='=' read -a L; do
+				[ "${L[0]}" == '_VERSION_' ] && V="${L[1]}"
+			done < "$F"
+
+			printf "%s=%s\n" "$F" "${V//\"}"
+		fi
+	}
+)"
+
+printf " [OK]\n"
+
+SAVE(){
+	if [ -f "$VER_FILE" ]; then
+		read -n 1 -e -p "[O]verwrite '$VER_FILE', or [q]uit? " OQ
+		case "$OQ" in
+			[Oo])
+				;;
+			[Qq])
+				exit 0 ;;
+			*)
+				FAIL 1 "$LINENO" "Invalid response -- quitting." ;;
+		esac
+	fi
+
+	printf "Writing data to: '%s'" "$VER_FILE"
+	printf "%s\n" "$DATA" > "$VER_FILE"
+	printf " [OK]\n"
+}
+
+read -n 1 -e -p "[V]iew, [s]ave, [b]oth, or [q]uit? " VSBQ
+case "$VSBQ" in
+	[Vv])
+		printf "\n%s\n" "$DATA" ;;
+	[Ss])
+		SAVE ;;
+	[Bb])
+		printf "\n%s\n\n" "$DATA"
+		SAVE ;;
+	[Qq])
+		exit 0 ;;
+	*)
+		FAIL 1 "$LINENO" "Invalid response -- quitting." ;;
+esac
