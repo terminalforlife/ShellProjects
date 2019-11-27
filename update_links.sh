8bf12133724a108538718ed70183719c88d871d4
terminalforlife <terminalforlife@yahoo.com>
2019-11-27 16:03:31 +0000

    Update and address questionable syntax

--- update_links.sh
+++ update_links.sh
@@ -1,38 +1,38 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 #----------------------------------------------------------------------------------
 # Project Name      - Extra/update_links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Mon 13 May 01:39:31 BST 2019
+# Last Change       - Wed 27 Nov 15:59:11 GMT 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
 # Just a simple, quick script to update the hard links when changing branches.
 #------------------------------------------------------------------------------MAIN
 
-FAIL(){
-	printf "[L%0.4d] ERROR: %s\n" "$2" "$3" 1>&2
+Err(){
+	printf "ERROR: %s\n" "$2" 1>&2
 	[ $1 -eq 1 ] && exit 1
 }
 
-declare -i DEPCOUNT=0
+DEPCOUNT=0
 for DEP in ln rm; {
 	if ! type -fP "$DEP" > /dev/null 2>&1; then
-		FAIL 0 "$LINENO" "Dependency '$DEP' not met."
+		Err 0 $LINENO "Dependency '$DEP' not met."
 		DEPCOUNT+=1
 	fi
 }
 
 [ $DEPCOUNT -eq 0 ] || exit 1
 
-if [ "${PWD//*\/}" != "Extra" ]; then
-	FAIL 1 "$LINENO" "Not in the repository's root directory."
+if [ "${PWD##*\/}" != "Extra" ]; then
+	Err 1 $LINENO "Not in the repository's root directory."
 else
 	rm -v $HOME/.config/compton.conf 2> /dev/null
 	ln -v compton.conf $HOME/.config/ 2> /dev/null
 
-	#rm -v $HOME/.keynavrc 2> /dev/null
-	#ln -v .keynavrc $HOME/ 2> /dev/null
+	rm -v $HOME/.keynavrc 2> /dev/null
+	ln -v .keynavrc $HOME/ 2> /dev/null
 
 	rm -v $HOME/.config/dunst/dunstrc 2> /dev/null
 	ln -v dunstrc $HOME/.config/dunst/ 2> /dev/null
