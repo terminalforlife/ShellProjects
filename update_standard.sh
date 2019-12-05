3cef75e0390c9f4dafb98ff5089453db234e9e36
terminalforlife <terminalforlife@yahoo.com>
2019-12-05 16:21:48 +0000

    Add `update_standard.sh` as a huge time-saver
    I've been gradually migrating to a new "standard" on which I've taken.
    This script which is essentially a load of `sed` commands, will save a
    lot of time and hassle.

---
+++ update_standard.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+#----------------------------------------------------------------------------------
+# Project Name      - Extra/update_standard.sh
+# Started On        - Thu  5 Dec 12:56:08 GMT 2019
+# Last Change       - Thu  5 Dec 16:21:35 GMT 2019
+# Author E-Mail     - terminalforlife@yahoo.com
+# Author GitHub     - https://github.com/terminalforlife
+#----------------------------------------------------------------------------------
+# Just a personal, temporary development tool to update the syntax standards of my
+# shell programs and small scripts. I have so many that this will be a time-saver.
+#
+# You could make use of some of this, though.
+#----------------------------------------------------------------------------------
+
+set -- "$HOME/GitHub/terminalforlife/Personal/Extra/lspkg"
+
+if ! grep -qE '^#!/(bin/bash|/usr/bin/env bash)$' "$1"; then
+	printf "ERROR: This script is strictly for those written with Bash." 1>&2
+	exit 1
+fi
+
+CurFile=$1
+shift
+
+SaR(){
+	if ! sed --posix -ri "$*" "$CurFile" &> /dev/null; then
+		printf "ERROR: %s\n" "$*" >&2
+		exit 1
+	fi
+}
+
+POSIXLY_CORRECT='y'
+
+# Prefer `CurVer` over `_VERSION`, and `Progrm` over `_PROJECT_`.
+SaR "s/^_VERSION_=\"(.*)\"/CurVer='\\1'/"
+SaR "s/^CurVer=\"(.*)\"/CurVer='\\1'/"
+SaR 's/_VERSION_/CurVer/g'
+SaR "s/^_PROJECT_=(.*)/Progrm=\\1/"
+SaR 's/^_PROJECT_="(\$\{0##\*\/\})"/Progrm=\1/'
+SaR 's/^Progrm="(\$\{0##\*\/\})"/Progrm=\1/'
+SaR 's/_PROJECT_/Progrm/g'
+
+# Needs to be corrected for my `todo` function, and for consistency.
+SaR 's/#TODO: /#TODO - /'
+
+# Prefer `Err()` over `FAIL()`.
+SaR 's/^FAIL(\(\)\{)/Err\1/'
+SaR 's/FAIL( [10]{1} )/Err\1/'
+SaR 's/"\$LINENO"/\$LINENO/g'
+
+# Use traditional case for function and variable names.
+SaR "s/^DOM=\"(.*)\"/Domain='\\1'/"
+SaR 's/\$DOM/$Domain/'
+SaR 's/USAGE\(\)\{/Usage\(\)\{/'
+SaR 's/USAGE\;/Usage\;/'
+SaR 's/\$DEBUGME/\$DebugMe/g'
+SaR 's/DEBUGME=/DebugMe=/g'
+
+# Remove expanded program/script title from `Usage()`; keeping it consistent.
+SaR 's/(\$\{Progrm\^\^\}) - .* (\(\$CurVer\))/\1 \2/'
+
+# Prefer Bash syntax for STDOUT and STDERR redirection to null.
+SaR 's|(1>\|>) /dev/null 2>&1|\&> /dev/null|g'
+
+# Update the header's timestamp.
+if Str=`grep -x '# Last Change\s\+- .*' "$CurFile"`; then
+	printf -v CurDate "%(%a %_d %b %T %Z %Y)T" -1
+	SaR "s/${Str#* - }$/$CurDate/"
+fi
+
+# Update the `CurVer` timestamp.
+if Str=`grep "^CurVer='.*" "$CurFile"`; then
+	printf -v CurDate "%(%F)T" -1
+	SaR "s/$Str/CurVer='$CurDate'/"
+fi
+
+# Remove unnecessary `-r` from `read` in `Usage()`.
+LineCount=0
+while read F1 F2 F3 _; do
+	let LineCount++
+
+	[ "$F1" == 'Usage(){' ] && IsUsage='true'
+	if [ "$IsUsage" == 'true' -a "$F1$F2$F3" == 'whileread-r;' ]; then
+		set -x
+		SaR "${LineCount}s/while read -r/while read/"
+		set +x
+		break
+	fi
+done < "$CurFile"
+
+# Correct no-longer-valid GitHub URL.
+SaR 's|(\$Domain/terminalforlife/)\$Progrm|\1Extra|g'
+
+# No quoting variables when using `[[`.
+SaR 's/\[\[ \"\$(.*)"/[[ $\1/g'
+
+# Appriately quote booleans.
+SaR "s/=\"([Tt][Rr][Uu][Ee])\"/='\\1'/g"
+SaR "s/ \"([Tt][Rr][Uu][Ee])\" / '\\1' /g"
+SaR "s/=\"([Ff][Aa][Ll][Ss][Ee])\"/='\\1'/g"
+SaR "s/ \"([Ff][Aa][Ll][Ss][Ee])\" / '\\1' /g"
+
+# Just in-case, albeit inaccurate, but integers are not strings.
+SaR "s/=[\"']{1}([0-9]*)[\"']{1}/=\\1/g"
