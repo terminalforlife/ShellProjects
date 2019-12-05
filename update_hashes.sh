01f2093fffd022c9796286562ebc453d4197e1ff
terminalforlife <terminalforlife@yahoo.com>
2019-12-05 20:35:32 +0000

    Add `update_hashes.sh` for use with `md5sum`

---
+++ update_hashes.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+#----------------------------------------------------------------------------------
+# Project Name      - Extra/update_hashes.sh
+# Started On        - Thu  5 Dec 19:36:09 GMT 2019
+# Last Change       - Thu  5 Dec 20:33:57 GMT 2019
+# Author E-Mail     - terminalforlife@yahoo.com
+# Author GitHub     - https://github.com/terminalforlife
+#----------------------------------------------------------------------------------
+# I wrote this to handle updating the md5sum of various files in this repository.
+# This script was written with Bourne POSIX-compliance in mind.
+#
+# You could refactor this to fit your needs, but as it is, I would leave it alone.
+#----------------------------------------------------------------------------------
+
+. ./libtflbp-sh
+
+SumFile='md5sum'
+
+if ! [ -r "$SumFile" -a -w "$SumFile" ]; then
+	Err 1 "The '$SumFile' file has insufficient permissions."
+fi
+
+ChkDep md5sum
+
+Excepts='
+	README.md
+	LICENSE
+	update_hashes.sh
+	update_links.sh
+	update_standard.sh
+	update_versions.sh
+'
+
+ChkSums(){
+	for CurFile in *; do
+		for CurExcept in $Excepts; do
+			[ "$CurExcept" = "$CurFile" ] && continue 2
+		done
+
+		[ -d "$CurFile" ] && continue
+		md5sum "$CurFile"
+	done
+}
+
+if YNInput 'Do you want to save the md5sums?'; then
+	ChkSums > "$SumFile"
+	if YNInput 'Add then commit the file changes?'; then
+		git add "$SumFile"
+
+		ComSum="Update '$SumFile' with new md5sums"
+		[ ${#ComSum} -lt 50 ] || Err 1 'Commit message exceeds maximum length.'
+
+		git commit -m "$ComSum"
+	fi
+else
+	ChkSums
+fi
