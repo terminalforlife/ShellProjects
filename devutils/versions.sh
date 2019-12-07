71e2ca4ce604efada69f0c80c651d7e5461fa880
terminalforlife <terminalforlife@yahoo.com>
2019-12-07 00:22:17 +0000

    Update and correct `versions.sh`

--- devutils/versions.sh
+++ devutils/versions.sh
@@ -1,71 +1,58 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 #----------------------------------------------------------------------------------
-# Project Name      - Extra/devutils/update_versions.sh
+# Project Name      - Extra/devutils/versions.sh
 # Started On        - Fri 25 Oct 12:41:34 BST 2019
-# Last Change       - Fri  6 Dec 03:30:04 GMT 2019
+# Last Change       - Sat  7 Dec 00:18:57 GMT 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
-# Fetches then saves versions deemed by current-directory shell prorams' initial
-# _VERSION_ variable, in key=value format. Parsing only; no program is executed.
-#----------------------------------------------------------------------------------
-
-echo "Don't use this yet! Needs to be updated..."
-
-exit
 
 . /usr/lib/tflbp-sh/Err
 . /usr/lib/tflbp-sh/ChkDep
 . /usr/lib/tflbp-sh/YNInput
 
-VER_FILE='./versions'
-
 ChkDep mimetype
 
-printf "%s " "Gathering version strings..."
+cd "$HOME/GitHub/terminalforlife/Personal/Extra"
 
-DATA="$(\
-	for F in *; {
-		MIME=`mimetype -b "$F" 2> /dev/null`
+VerFile='versions'
 
-		for EXCEPT in ${EXCEPTIONS[@]}; {
-			[ "$F" == "$EXCEPT" ] && continue 2
-		}
+YNInput "Update the contents of '$VerFile' file?"\
+	&& DoUpdate='true'
 
-		if [[ $MIME == *x-shellscript ]]; then
-			while IFS='=' read -a L; do
-				[ "${L[0]}" == '_VERSION_' ] && V=${L[1]}
-			done < "$F"
+1> "$VerFile"
+for CurFile in source/*; do
+	[ -f "$CurFile" ] || continue
 
-			printf "%s=%s\n" "$F" "${V//\"}"
+	FileType=`mimetype -b "$CurFile"`
+
+	if [ "${FileType#*/}" = 'x-shellscript' ]; then
+		while IFS='=' read Key Value; do
+			if [ "$Key" = 'CurVer' -o "$Key" = '_VERSION_' ]; then
+				Version=${Value#?}
+				Version=${Version%?}
+
+				case $Version in
+					[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])
+						break ;;
+					*)
+						Err 0 "Invalid '$Key' value of '$Value'."
+						continue 2 ;; # <-- Don't add this file to `versions`.
+				esac
+			fi
+		done < "$CurFile"
+
+		if [ "$DoUpdate" = 'true' ]; then
+			printf "${CurFile##*/}=$Version\n" >> "$VerFile"
+		else
+			printf "${CurFile##*/}=$Version\n"
 		fi
-	}
-)"
-
-printf " [OK]\n"
-
-SAVE(){
-	if [ -f "$VER_FILE" ]; then
-		YNInput "Overwrite '$VER_FILE' file?" || exit 0
 	fi
+done
 
-	printf "Writing data to: '%s'" "$VER_FILE"
-	printf "%s\n" "$DATA" > "$VER_FILE"
-	printf " [OK]\n"
-}
+YNInput "Add these changes to the staging area?"\
+	&& git add "$VerFile"
 
-read -n 1 -e -p "[V]iew, [s]ave, [b]oth, or [q]uit? " VSBQ
-case $VSBQ in
-	[Vv])
-		printf "\n%s\n" "$DATA" ;;
-	[Ss])
-		SAVE ;;
-	[Bb])
-		printf "\n%s\n\n" "$DATA"
-		SAVE ;;
-	[Qq])
-		exit 0 ;;
-	*)
-		Err 1 "Invalid response -- quitting." ;;
-esac
+YNInput "Commit these and any other changes, now?"\
+	&& git commit -m 'Update `'"$VerFile"'`'
