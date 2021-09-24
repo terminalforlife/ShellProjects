72e05208a11bbd277861b1d06d5b6af372e56834
terminalforlife <terminalforlife@yahoo.com>
2021-09-24 22:20:25 +0100

    Update and tweak links.sh

--- devutils/links.sh
+++ devutils/links.sh
@@ -3,46 +3,38 @@
 #------------------------------------------------------------------------------
 # Project Name      - Extra/devutils/links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Wed 11 Nov 17:37:37 GMT 2020
+# Last Change       - Fri 24 Sep 22:20:14 BST 2021
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
 # Just a simple, quick script to update the hard links when changing branches.
 #------------------------------------------------------------------------------
 
-{
-	if cd "$HOME/GitHub/terminalforlife/Personal/Extra"; then
-		mkdir -v "$HOME/.config"
+exec 2> /dev/null
 
-		if command -v compton 1> /dev/null; then
-			rm -v $HOME/.config/compton.conf
-			ln -v misc/compton.conf $HOME/.config/
-		fi
+if cd "$HOME/GitHub/terminalforlife/Personal/Extra"; then
+	mkdir -pv "$HOME/.config"
 
-		if command -v keynav 1> /dev/null; then
-			rm -v $HOME/.keynavrc
-			ln -v misc/.keynavrc $HOME/
-		fi
-
-		if command -v dunst 1> /dev/null; then
-			mkdir -v "$HOME/.config/dunst"
-
-			rm -v $HOME/.config/dunst/dunstrc
-			ln -v misc/dunstrc $HOME/.config/dunst/
-		fi
-
-		if command -v xfce4-terminal 1> /dev/null; then
-			mkdir -v "$HOME/.config/xfce4/terminal"
-
-			rm -v $HOME/.config/xfce4/terminal/terminalrc
-			ln -v misc/terminalrc $HOME/.config/xfce4/terminal/terminalrc
-		fi
-
-		if command -v herbsluftwm 1> /dev/null; then
-			mkdir -v "$HOME/.config/herbstluftwm"
-
-			rm -v $HOME/.config/herbstluftwm/autostart
-			ln -v misc/autostart $HOME/.config/herbstluftwm/
-		fi
+	if command -v compton 1> /dev/null; then
+		ln -fv misc/compton.conf $HOME/.config/
 	fi
-} 2> /dev/null
+
+	if command -v keynav 1> /dev/null; then
+		ln -fv misc/.keynavrc $HOME/
+	fi
+
+	if command -v dunst 1> /dev/null; then
+		mkdir -pv "$HOME/.config/dunst"
+		ln -fv misc/dunstrc $HOME/.config/dunst/
+	fi
+
+	if command -v xfce4-terminal 1> /dev/null; then
+		mkdir -pv "$HOME/.config/xfce4/terminal"
+		ln -fv misc/terminalrc $HOME/.config/xfce4/terminal/terminalrc
+	fi
+
+	if command -v herbsluftwm 1> /dev/null; then
+		mkdir -pv "$HOME/.config/herbstluftwm"
+		ln -fv misc/autostart $HOME/.config/herbstluftwm/
+	fi
+fi
