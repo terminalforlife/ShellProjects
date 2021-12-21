ae312d028507f80b02aae5ecd261419e2c14417e
terminalforlife <terminalforlife@yahoo.com>
2021-12-21 17:00:51 +0000

    Compile all (non-alpha/beta) versions of BASH
    Finally, this script does what I now want. Drawback? Determining the
    order of versions is now going to be a huge pain, because of the
    variety of version strings. I'm thinking of parsing the site's
    timestamps for each tarball, but I doubt that'll be too reliable, as
    a tarball could've been changed post-release.

--- devutils/compile-bash.sh
+++ devutils/compile-bash.sh
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 # Project Name      - Extra/devutils/compile-bash.sh
 # Started On        - Mon  6 Dec 00:18:01 GMT 2021
-# Last Change       - Tue 21 Dec 01:39:58 GMT 2021
+# Last Change       - Tue 21 Dec 16:59:50 GMT 2021
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
@@ -16,11 +16,9 @@
 #
 # WARNING: This script does NOT (yet?) verify the signature of the tarball.
 #
-#          N.N.N releases and `rc` releases are not included, yet.
-#
 # Features:
 #
-# N/A
+#TODO: Maybe use the timestamp of the remote tarball to determine list order.
 #
 # Bugs:
 #
@@ -71,79 +69,30 @@ while read; do
 	# don't reuse the same captured group number.
 	File=${BASH_REMATCH[1]}
 
-	[[ $File =~ ^bash-[[:digit:]]+\.[[:digit:]]+\.tar\.gz$ ]] && Files+=("$File")
+	case $File in
+		bash-[0-9]*.tar.gz)
+			case $File in
+				*alpha*|*beta*)
+					;;
+				*)
+					Files+=("$File") ;;
+			esac ;;
+	esac
 done <<< "$(wget -qO - "$URL")"
-
-# Re-order the old-to-new sorted list of files to new-to-old.
-TotalFiles=${#Files[@]}
-for (( Start = TotalFiles - 1; Start >= 0; Start-- )); {
-	SortedFiles+=("${Files[Start]}")
-}
-
-unset Files
-
-#--------------------------------------------Get Target Tarball from User Input
-
-printf 'Please \e[93mselect\e[0m a tarball of BASH to use.\n\n'
-
-Count=1
-for File in "${SortedFiles[@]}"; {
-	(( Count > 10 )) && break
-
-	printf '  %4s  %s\n' "($Count)" "$File"
-	(( Count++ ))
-}
-
-unset Count File
-
-printf '\n'
-
-while :; do
-	read -p '> ' GetVersion
-
-	if [ -z "$GetVersion" ]; then
-		Err 0 'Null response.'
-		continue
-	elif ! [[ $GetVersion =~ ^([123456789]|10)+$ ]]; then
-		Err 0 'Invalid response.'
-		continue
-	fi
-
-	ChosenFile=${SortedFiles[GetVersion - 1]}
-	ChosenVersion=${ChosenFile#*-}
-	ChosenVersion=${ChosenVersion%.tar.gz}
-	printf 'You selected \e[92mBASH %s\e[0m -- continue?\n' "$ChosenVersion"
-	while :; do
-		read -p '> ' Confirm
-
-		case $Confirm in
-			[Yy]|[Yy][Ee][Ss])
-				Happy='True'
-				break ;;
-			[Nn]|[Nn][Oo])
-				Happy='False'
-				break ;;
-			'')
-				Err 0 'Null response.' ;;
-			*)
-				Err 0 'Invalid response.' ;;
-		esac
-	done
-
-	[ "$Happy" == 'True' ] && break
-done
-
-unset SortedFiles Happy GetVersion Confirm REPLY
 
 #--------------------------------------------------------Configure Then Compile
 
-if wget -q --show-progress "$URL/$ChosenFile"; then
-	tar -xzvf "$ChosenFile" && rm -v "$ChosenFile"
+for BASH in "${Files[@]}"; {
+	[ -d "${BASH%.tar.gz}" ] && continue
 
-	if cd "${ChosenFile%.tar.gz}"; then
-		./configure
-		make -j $Cores
+	if wget -q --show-progress "$URL/$BASH"; then
+		tar -xzvf "$BASH" && rm -v "$BASH"
+
+		(
+			if cd "${BASH%.tar.gz}"; then
+				./configure
+				make -j $Cores
+			fi
+		)
 	fi
-
-	printf 'Done!\n'
-fi
+}
