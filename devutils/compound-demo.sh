878f787879f05cb3f9de99d9762897527dcfc8c6
terminalforlife <terminalforlife@yahoo.com>
2023-06-02 18:02:21 +0100

    Overhaul compound-demo.sh (a decent solution)

--- devutils/compound-demo.sh
+++ devutils/compound-demo.sh
@@ -3,127 +3,61 @@
 #------------------------------------------------------------------------------
 # Project Name      - Extra/devutils/compound-demo.sh
 # Started On        - Sun  7 Mar 22:26:18 GMT 2021
-# Last Change       - Sun  7 Mar 22:53:39 GMT 2021
+# Last Change       - Fri  2 Jun 18:01:37 BST 2023
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
-# For all these years, there's been a big caveat to my programs, and that's the
-# inability to use compounded short-form arguments, such as `-this` being equal
-# to `-t`, `-h`, `-i`, and `-s`.
+# As of 2023-06-02, I annoyingly easily conjured up this far more concise and
+# practical solution to my very old previous approach. This looks to be so good
+# that I will probably implement it into all of my BASH projects. This sort of
+# approach would never have occurred to me a couple of years ago.
 #
-# However, with this code, I've demonstrated how it can work, at least in BASH.
-# In Perl, I'd do something similar, but certainly much cleaner.
-#
-# The downside, is that I'd have to use functions or something for each
-# argument with a long and short form; this would add complexity and add a lot
-# more code, but it'd work!
-#
-# Another problem with this approach, is that it messes up the order in which
-# the user provides the arguments to the script; it's probably not a huge deal
-# but it could really put a spanner in the works of some programs.
-#
-# One workaround for the need for multiple additional functions, would be to
-# use just the one additional function whose arguments can be provided as the
-# arguments to the actual script.
-#
-# Honestly, I'd probably resort to `getopts` before I used this approach.
+# These example flags are taken (neutered) from CSi3.
 #------------------------------------------------------------------------------
 
-# Uncomment the below lines for an easy demonstration.
-#printf 'ARGS: --this --is -ate --st\n'
-#set -- --this --is -ate --st
-
-CurVer='2021-03-07'
-Progrm=${0##*/}
-
-Err(){
-	printf 'ERROR: %s\n' "$2" 1>&2
-	[ $1 -gt 0 ] && exit $1
-}
-
-ArgUsage() {
-	while read; do
-		printf '%s\n' "$REPLY"
-	done <<-EOF
-		Usage: $Progrm [OPTS]
-
-		  -h, --help               - Display this help information.
-		  -v, --version            - Output the version datestamp.
-		  --is                     - ???
-		  --st                     - ???
-		  --this                   - ???
-		  -a                       - ???
-		  -e                       - ???
-		  -t, --test               - ???
-	EOF
-
-	exit 0
-}
-
-ArgVersion() {
-	printf '%s\n' "$CurVer"
-	exit 0
-}
-
-ArgTest() {
-	printf "Found the '--test|-t' flag.\n"
-}
-
-# First, process the long-form arguments.
-while [ "$1" ]; do
+Args=()
+while [[ -n $1 ]]; do
 	case $1 in
-		--help)
-			ArgUsage ;;
-		--version)
-			ArgVersion ;;
-		--this)
-			printf 'Found: --this\n' ;;
-		--is)
-			printf 'Found: --is\n' ;;
-		--st)
-			printf 'Found: --st\n' ;;
-		--test)
-			ArgTest ;;
-		-[!-]*)
-			# If a series of short-form arguments, are found, parse them, then
-			# store them into a separate array which can be processed later.
-			#
-			# The downside, is that the last short-form argument doesn't allow
-			# the use of an argument, but it could be incorporated using an
-			# additional `shift` call based on what the last short argument is.
-			for Char in `eval printf '%d\\\n' {1..${#1}}`; {
-				Short="-${1:$Char:1}"
-				if [ "$Short" == '-' ]; then
-					continue
-				else
-					Shorts+=("$Short")
-				fi
+		-[^-]*)
+			Str=${1#-}
+			Len=${#Str}
+			for (( Index = 0; Index < Len; Index++ )); {
+				Char=${Str:Index:1}
+				Args+=(-"$Char")
 			} ;;
-		--*)
-			Err 1 'Invalid option(s) specified.' ;;
+		*)
+			Args+=("$1") ;;
 	esac
 	shift
 done
 
-# Yet another downside to this block being separate from the above, is that if
-# duplicates are provided, you'll wind up re-executing the code via the called
-# function. Run `$0 -te -t` to see what I mean.
-#
-# This could be addressed by using a variable, but it'd be yet more complexity
-# and inconvenience.
-for Arg in "${Shorts[@]}"; do
-	case $Arg in
-		-h)
-			ArgUsage ;;
-		-v)
-			ArgVersion ;;
-		-a)
-			printf 'Found: -a\n' ;;
-		-t)
-			ArgTest ;;
-		-e)
-			printf 'Found: -e\n' ;;
-		-*)
-			Err 1 'Invalid option(s) specified.' ;;
+set -- "${Args[@]}"
+
+while [[ -n $1 ]]; do
+	case $1 in
+		--help|-h|-\?)
+			printf '%s\n' "$1" ;;
+		--version|-v)
+			printf '%s\n' "$1" ;;
+		--nocolor|-C)
+			printf '%s\n' "$1" ;;
+		--noexecs|-E)
+			printf '%s\n' "$1" ;;
+		--ignore-vars|-V)
+			printf '%s\n' "$1" ;;
+		--execs|-e)
+			printf '%s\n' "$1" ;;
+		--key|-k)
+			printf '%s\n' "$1" ;;
+		--file|-f)
+			printf '%s\n' "$1" ;;
+		--number|-n)
+			printf '%s\n' "$1" ;;
+		-)
+			printf '%s\n' "$1" ;;
+		*)
+			printf 'Err: Incorrect argument(s) specified.\n' 1>&2
+			exit 1 ;;
 	esac
+	shift
 done
